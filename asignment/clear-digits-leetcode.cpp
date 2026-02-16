4e5bb439403f14a2436127cafddc17e3771cf9d8
Shreya <shreyam484@gmail.com>
2026-02-16 09:51:41 +0530

    add practice problems

---
+++ asignment/clear-digits-leetcode.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        
+        stack<char> st;
+        
+        for (char ch : s) {
+            
+            if (isdigit(ch)) {
+                if (!st.empty()) 
+                    st.pop();   
+            }
+            else {
+                st.push(ch);   
+            }
+        }
+        
+        string result = "";
+        
+        while (!st.empty()) {
+            result += st.top();
+            st.pop();
+        }
+        
+        reverse(result.begin(), result.end());
+        
+        return result;
+    }
+};
---
+++ practice/implement-stack-using-queue-leetcode.cpp
@@ -0,0 +1,49 @@
+class MyStack {
+public:
+    queue<int> q1, q2;
+
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q1.push(x);  
+    }
+    
+    int pop() {
+        
+        while(q1.size() > 1) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        
+        int topElement = q1.front();
+        q1.pop();
+        
+       
+        swap(q1, q2);
+        
+        return topElement;
+    }
+    
+    int top() {
+       
+        while(q1.size() > 1) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        
+        int topElement = q1.front();
+        q2.push(q1.front());  
+        q1.pop();
+        
+        
+        swap(q1, q2);
+        
+        return topElement;
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
